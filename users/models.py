81973d7b1129fdca10d5deb31cdd70c4048d2069
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-10-21 16:30:40 +0900

    Finishing User Model

--- users/models.py
+++ users/models.py
@@ -16,8 +16,36 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_JAPANESE = "jp"
+
+    LANGUAGE_CHOICE = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_JAPANESE, "Japanese"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_JPY = "jpy"
+
+    CURRENCY_CHOICE = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_JPY, "JPY"),
+    )
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.TextField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
