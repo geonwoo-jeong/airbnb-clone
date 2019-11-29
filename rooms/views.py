310a37360ca826420fb12347ff08948e93142a5e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-11-29 20:32:52 +0900

    Filtering Like a Boss part Two

--- rooms/views.py
+++ rooms/views.py
@@ -39,13 +39,13 @@ class RoomDetail(DetailView):
 
 def search(request):
     city = request.GET.get("city", "Anywhere")
-    price = request.GET.get("price", 0)
-    guests = request.GET.get("guests", 0)
-    bedrooms = request.GET.get("bedrooms", 0)
-    baths = request.GET.get("baths", 0)
-    beds = request.GET.get("beds", 0)
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+    beds = int(request.GET.get("beds", 0))
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
 
     selected_country = request.GET.get("country", "JP")
     selected_room_type = request.GET.get("room_type", 0)
@@ -66,7 +66,7 @@ def search(request):
         "baths": int(baths),
         "beds": int(beds),
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
         "selected_room_type": int(selected_room_type),
         "selected_country": selected_country,
         "selected_amenities": selected_amenities,
@@ -87,6 +87,35 @@ def search(request):
 
     if selected_room_type != 0:
         filter_args["room_type__pk__exact"] = selected_room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(selected_amenities) > 0:
+        for selected_amenity in selected_amenities:
+            filter_args["amenities__pk"] = int(selected_amenity)
+
+    if len(selected_facilities) > 0:
+        for selected_facility in selected_facilities:
+            filter_args["facilities_pk"] = int(selected_facility)
 
     filter_args["country"] = selected_country
 
