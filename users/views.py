ab67561c1cf91b04ce9693b32d5d9cedd29ab3ec
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-12-18 22:34:12 +0900

    Log In and Log Out

--- users/views.py
+++ users/views.py
@@ -1,13 +1,33 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 class LoginView(View):
     def get(self, request):
         form = forms.LoginForms()
+
         return render(request, "users/user_login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForms(request.POST)
-        print(form)
+
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect("core:home")
+
+        return render(request, "users/user_login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+
+    return redirect("core:home")
+
