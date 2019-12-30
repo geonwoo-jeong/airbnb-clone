c701677f04b3bcca280b901093be4891dd6c62c7
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-12-30 14:44:09 +0900

    Add Line Login

--- users/views.py
+++ users/views.py
@@ -69,6 +69,88 @@ class GithubException(Exception):
     pass
 
 
+class LineException(Exception):
+    pass
+
+
+def line_login(self):
+    response_type = "code"
+    client_id = os.environ.get("LINE_CHANNEL")
+    redirect_uri = HttpRequest.build_absolute_uri(self, reverse("users:line-callback"))
+    state = "asjdklqlkfj"
+    scope = "openid%20profile"
+
+    return redirect(
+        f"https://access.line.me/oauth2/v2.1/authorize?response_type={response_type}&client_id={client_id}&state={state}&scope={scope}&nonce=asdasd&redirect_uri={redirect_uri}"
+    )
+
+
+def line_callback(request):
+    try:
+        client_id = os.environ.get("LINE_CHANNEL")
+        client_secret = os.environ.get("LINE_SECRET")
+        grant_type = "authorization_code"
+        code = request.GET.get("code", None)
+        state = request.GET.get("state", None)
+        redirect_uri = "http://localhost:8000/users/login/line/callback"
+
+        # Get Token
+
+        token_request = requests.post(
+            "https://api.line.me/oauth2/v2.1/token",
+            data={
+                "grant_type": grant_type,
+                "client_id": client_id,
+                "client_secret": client_secret,
+                "code": code,
+                "state": state,
+                "redirect_uri": redirect_uri,
+                "friendship_status_changed": "true",
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        token_json = token_request.json()
+
+        # Get User Information
+
+        user_info_request = requests.post(
+            "https://api.line.me/oauth2/v2.1/verify",
+            data={"id_token": token_json.get("id_token"), "client_id": client_id},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        user_info_json = user_info_request.json()
+
+        # Set User Information
+
+        try:
+            sub = user_info_json.get("sub")
+            name = user_info_json.get("name")
+            user = models.User.objects.get(username=sub)
+
+            if user.login_method != models.User.LOGIN_LINE:
+                raise LineException(f"Please log in with: {user.login_method}")
+
+        except models.User.DoesNotExist:
+            user = models.User.objects.create(
+                first_name=name,
+                username=sub,
+                login_method=models.User.LOGIN_LINE,
+                email_verified=False,
+            )
+
+            user.set_unusable_password()
+            user.save()
+
+        login(request, user)
+        return redirect(reverse("core:home"))
+
+    except LineException as e:
+        print(e)
+        return redirect(reverse("core:home"))
+
+
 def github_login(self):
     client_id = os.environ.get("GITHUB_ID")
     redirect_uri = HttpRequest.build_absolute_uri(
@@ -147,7 +229,7 @@ def github_callback(request):
 
     except GithubException as e:
         print(e)
-        return redirect(reverse("users:login"))
+        return redirect("users:login")
 
 
 # class LoginView(View):
